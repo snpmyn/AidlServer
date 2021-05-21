886ac17ac0eae7f0038fd670d4912e117c27d304
snpmyn <snpmyn@126.com>
2021-05-21 09:43:50 +0800

    Initial commit

---
+++ app/src/main/aidl/com/zsp/aidlserver/CalculateAIDL.aidl
@@ -0,0 +1,10 @@
+// CalculateAIDL.aidl
+package com.zsp.aidlserver;
+
+// Declare any non-default types here with import statements
+
+interface CalculateAIDL {
+    int add(int x, int y);
+    int max(int x, int y);
+    int min(int x, int y);
+}
---
+++ app/src/main/java/com/zsp/aidlserver/MainActivity.java
@@ -0,0 +1,19 @@
+package com.zsp.aidlserver;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @desc: 主页
+ * @author: zsp
+ * @date: 2021/5/20 4:26 下午
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
