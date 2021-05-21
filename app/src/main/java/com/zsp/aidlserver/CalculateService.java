631a01f7e3e9e1701339b58d3451f8ceae9f84ca
snpmyn <snpmyn@126.com>
2021-05-21 10:02:24 +0800

    优化

--- app/src/main/java/com/zsp/aidlserver/CalculateService.java
+++ app/src/main/java/com/zsp/aidlserver/CalculateService.java
@@ -1,9 +1,7 @@
 package com.zsp.aidlserver;
 
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -37,72 +35,18 @@ public class CalculateService extends Service {
         }
     };
 
-    /**
-     * Called by the system when the service is first created.  Do not call this method directly.
-     */
     @Override
     public void onCreate() {
         Log.d(TAG, "onCreate");
         super.onCreate();
     }
 
-    /**
-     * Called by the system every time a client explicitly starts the service by calling
-     * {@link Context#startService}, providing the arguments it supplied and a
-     * unique integer token representing the start request.  Do not call this method directly.
-     *
-     * <p>For backwards compatibility, the default implementation calls
-     * {@link #onStart} and returns either {@link #START_STICKY}
-     * or {@link #START_STICKY_COMPATIBILITY}.
-     *
-     * <p class="caution">Note that the system calls this on your
-     * service's main thread.  A service's main thread is the same
-     * thread where UI operations take place for Activities running in the
-     * same process.  You should always avoid stalling the main
-     * thread's event loop.  When doing long-running operations,
-     * network calls, or heavy disk I/O, you should kick off a new
-     * thread, or use {@link AsyncTask}.</p>
-     *
-     * @param intent  The Intent supplied to {@link Context#startService},
-     *                as given.  This may be null if the service is being restarted after
-     *                its process has gone away, and it had previously returned anything
-     *                except {@link #START_STICKY_COMPATIBILITY}.
-     * @param flags   Additional data about this start request.
-     * @param startId A unique integer representing this specific request to
-     *                start.  Use with {@link #stopSelfResult(int)}.
-     * @return The return value indicates what semantics the system should
-     * use for the service's current started state.  It may be one of the
-     * constants associated with the {@link #START_CONTINUATION_MASK} bits.
-     * @see #stopSelfResult(int)
-     */
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(TAG, "onStartCommand");
         return super.onStartCommand(intent, flags, startId);
     }
 
-    /**
-     * Return the communication channel to the service.  May return null if
-     * clients can not bind to the service.  The returned
-     * {@link IBinder} is usually for a complex interface
-     * that has been <a href="{@docRoot}guide/components/aidl.html">described using
-     * aidl</a>.
-     *
-     * <p><em>Note that unlike other application components, calls on to the
-     * IBinder interface returned here may not happen on the main thread
-     * of the process</em>.  More information about the main thread can be found in
-     * <a href="{@docRoot}guide/topics/fundamentals/processes-and-threads.html">Processes and
-     * Threads</a>.</p>
-     *
-     * @param intent The Intent that was used to bind to this service,
-     *               as given to {@link Context#bindService
-     *               Context.bindService}.  Note that any extras that were included with
-     *               the Intent at that point will <em>not</em> be seen here.
-     * @return Return an IBinder through which clients can call on to the
-     * service.
-     * <p>
-     * 返回上面实现 AIDL 接口方法的实例。
-     */
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -110,47 +54,18 @@ public class CalculateService extends Service {
         return stub;
     }
 
-    /**
-     * Called when new clients have connected to the service, after it had
-     * previously been notified that all had disconnected in its
-     * {@link #onUnbind}.  This will only be called if the implementation
-     * of {@link #onUnbind} was overridden to return true.
-     *
-     * @param intent The Intent that was used to bind to this service,
-     *               as given to {@link Context#bindService
-     *               Context.bindService}.  Note that any extras that were included with
-     *               the Intent at that point will <em>not</em> be seen here.
-     */
     @Override
     public void onRebind(Intent intent) {
         Log.d(TAG, "onRebind");
         super.onRebind(intent);
     }
 
-    /**
-     * Called when all clients have disconnected from a particular interface
-     * published by the service.  The default implementation does nothing and
-     * returns false.
-     *
-     * @param intent The Intent that was used to bind to this service,
-     *               as given to {@link Context#bindService
-     *               Context.bindService}.  Note that any extras that were included with
-     *               the Intent at that point will <em>not</em> be seen here.
-     * @return Return true if you would like to have the service's
-     * {@link #onRebind} method later called when new clients bind to it.
-     */
     @Override
     public boolean onUnbind(Intent intent) {
         Log.d(TAG, "onUnbind");
         return super.onUnbind(intent);
     }
 
-    /**
-     * Called by the system to notify a Service that it is no longer used and is being removed.  The
-     * service should clean up any resources it holds (threads, registered
-     * receivers, etc) at this point.  Upon return, there will be no more calls
-     * in to this Service object and it is effectively dead.  Do not call this method directly.
-     */
     @Override
     public void onDestroy() {
         Log.d(TAG, "onDestroy");
